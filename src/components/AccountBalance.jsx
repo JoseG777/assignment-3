9d506f837d81d59c76e98c62578533b9409523bf
JoseG777 <104945899+JoseG777@users.noreply.github.com>
2024-04-07 18:02:17 -0400

    account balance set up

---
+++ src/components/AccountBalance.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AccountBalance = ({ balance }) => {
+    return (
+        <>
+            <h1>
+                Your current balance is ${balance}
+            </h1>
+        </>
+    )
+}
+export default AccountBalance;
