091e4106f4c02e7b9059411158dc98f8b4f44332
JoseG777 <104945899+JoseG777@users.noreply.github.com>
2024-04-07 17:59:08 -0400

    login page set up

---
+++ src/components/Login.jsx
@@ -0,0 +1,13 @@
+import e from "express";
+import React from "react";
+
+//<Route path="/login" element={<Login mockLogIn={mockLogIn} />} />
+function Login({ mockLogIn }) {
+    return (
+        <div>
+            <h1>Login</h1>
+        </div>
+    );
+}
+
+export default Login;
