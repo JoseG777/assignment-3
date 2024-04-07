15827167b76b57a2d02e2074b81ded84f68a625d
JoseG777 <104945899+JoseG777@users.noreply.github.com>
2024-04-07 17:50:28 -0400

    basic set up

---
+++ src/components/Credit.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Credit({ updateCredits, index }) {
+    return (
+        <div>
+            <p>{credit.description}</p>
+            <p>{credit.amount}</p>
+            <p>{credit.date}</p>
+        </div>
+    );
+}
+
+export default Credit;
