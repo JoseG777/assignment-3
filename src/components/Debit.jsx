8e9ea0111d6166d2717a9d4aa965193cb581d1d0
JoseG777 <104945899+JoseG777@users.noreply.github.com>
2024-04-07 17:57:48 -0400

    debits set up

---
+++ src/components/Debit.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Debits({ updateDebits, balance }) {
+    return (
+        <>
+            <h1> Debits </h1>
+        </>
+    );
+}
+
+export default Debits;
