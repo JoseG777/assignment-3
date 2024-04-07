7606742b12656e99b7cd3e365f971e25379b3517
JoseG777 <104945899+JoseG777@users.noreply.github.com>
2024-04-07 18:33:17 -0400

    use profile updated

--- src/components/UserProfile.jsx
+++ src/components/UserProfile.jsx
@@ -3,9 +3,9 @@ import React from 'react';
 const UserProfile = ({ userName, memberSince }) => {
     return (
         <>
-            <h1>User Profile</h1>
-            <p>Username : {userName}</p>
-            <p>Member Since : {memberSince}</p>
+            <h1> User Profile </h1>
+            <p> Username : {userName} </p>
+            <p> Member Since : {memberSince} </p>
         </>
     )
 }
